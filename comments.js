2650f99bd0500d33d383d79d728225ea6043cd39
mplietsch <102160121+mplietsch@users.noreply.github.com>
2023-07-24 19:18:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,79 +1,88 @@
-// create web server
-// create database connection
-// create schema
-// create model
-// create route
-// create view
-// create controller
-// create middleware
-// create test
-// create documentation
-// create logger
-
-// 1. create web server
+//create web server
 const express = require('express');
-const app = express();
-const port = 3000;
-
-// 2. create database connection
-const mongoose = require('mongoose');
-const db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function () {
-  console.log('Connected to mongod server');
-});
-mongoose.connect('mongodb://localhost/mongodb_tutorial');
-
-// 3. create schema
-const commentSchema = mongoose.Schema({
-  name: String,
-  comment: String,
-  created_at: { type: Date, default: Date.now },
-});
-
-// 4. create model
-const Comment = mongoose.model('comment', commentSchema);
-
-// 5. create route
-// 5-1. index
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
-// 5-2. comment list
-app.get('/comments', function (req, res) {
-  Comment.find(function (err, comments) {
-    if (err) return res.status(500).send({ error: 'database failure' });
-    res.json(comments);
-  });
-});
-
-// 5-3. comment show
-app.get('/comments/:comment_id', function (req, res) {
-  Comment.findOne({ _id: req.params.comment_id }, function (err, comment) {
-    if (err) return res.status(500).json({ error: err });
-    if (!comment) return res.status(404).json({ error: 'comment not found' });
-    res.json(comment);
-  });
-});
-
-// 5-4. comment create
-app.post('/comments', function (req, res) {
-  const comment = new Comment();
-  comment.name = req.body.name;
-  comment.comment = req.body.comment;
-  comment.save(function (err) {
-    if (err) {
-      console.error(err);
-      res.json({ result: 0 });
-      return;
-    }
-    res.json({ result: 1 });
-  });
-});
-
-// 5-5. comment update
-app.put('/comments/:comment_id', function (req, res) {
-  Comment.findById(req.params.comment_id, function (err, comment) {
-    if (err)
-        return res.status(500).json({ error: 'database failure' });});});
+//create a router object
+const router = express.Router();
+//import db module
+const db = require('../models');
+//import middleware
+const flash = require('connect-flash');
+const passport = require('../config/ppConfig');
+const isLoggedIn = require('../middleware/isLoggedIn');
+const methodOverride = require('method-override');
+//create routes
+router.get('/', (req, res) => {
+    db.comment.findAll({
+        include: [db.user]
+    })
+    .then(comments => {
+        res.render('comments/index', { comments })
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('Error')
+    })
+})
+//GET /comments/new
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('comments/new')
+})
+//POST /comments
+router.post('/', isLoggedIn, (req, res) => {
+    db.comment.create({
+        title: req.body.title,
+        content: req.body.content,
+        userId: req.user.id
+    })
+    .then(comment => {
+        console.log('Created comment', comment)
+        res.redirect('/comments')
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('404')
+    })
+})
+//GET /comments/:id
+router.get('/:id', (req, res) => {
+    db.comment.findOne({
+        where: { id: req.params.id },
+        include: [db.user]
+    })
+    .then(comment => {
+        console.log(comment)
+        res.render('comments/show', { comment })
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('404')
+    })
+})
+//DELETE /comments/:id
+router.delete('/:id', (req, res) => {
+    db.comment.destroy({
+        where: { id: req.params.id }
+    })
+    .then(() => {
+        console.log('Deleted comment')
+        res.redirect('/comments')
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('404')
+    })
+})
+//GET /comments/:id/edit
+router.get('/:id/edit', (req, res) => {
+    db.comment.findOne({
+        where: { id: req.params.id }
+    })
+    .then(comment => {
+        console.log(comment)
+        res.render('comments/edit', { comment })
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('404')
+    })
+})
+//
