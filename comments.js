46a39a02e7ae708e3afd0e5b49c32969d7093f2d
mplietsch <102160121+mplietsch@users.noreply.github.com>
2023-07-24 19:20:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,88 +1,41 @@
-//create web server
-const express = require('express');
-//create a router object
-const router = express.Router();
-//import db module
-const db = require('../models');
-//import middleware
-const flash = require('connect-flash');
-const passport = require('../config/ppConfig');
-const isLoggedIn = require('../middleware/isLoggedIn');
-const methodOverride = require('method-override');
-//create routes
-router.get('/', (req, res) => {
-    db.comment.findAll({
-        include: [db.user]
-    })
-    .then(comments => {
-        res.render('comments/index', { comments })
-    })
-    .catch(err => {
-        console.log(err)
-        res.send('Error')
-    })
-})
-//GET /comments/new
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('comments/new')
-})
-//POST /comments
-router.post('/', isLoggedIn, (req, res) => {
-    db.comment.create({
-        title: req.body.title,
-        content: req.body.content,
-        userId: req.user.id
-    })
-    .then(comment => {
-        console.log('Created comment', comment)
-        res.redirect('/comments')
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('404')
-    })
-})
-//GET /comments/:id
-router.get('/:id', (req, res) => {
-    db.comment.findOne({
-        where: { id: req.params.id },
-        include: [db.user]
-    })
-    .then(comment => {
-        console.log(comment)
-        res.render('comments/show', { comment })
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('404')
-    })
-})
-//DELETE /comments/:id
-router.delete('/:id', (req, res) => {
-    db.comment.destroy({
-        where: { id: req.params.id }
-    })
-    .then(() => {
-        console.log('Deleted comment')
-        res.redirect('/comments')
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('404')
-    })
-})
-//GET /comments/:id/edit
-router.get('/:id/edit', (req, res) => {
-    db.comment.findOne({
-        where: { id: req.params.id }
-    })
-    .then(comment => {
-        console.log(comment)
-        res.render('comments/edit', { comment })
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('404')
-    })
-})
-//
+// Create web server
+// 1. create a web server
+// 2. create a route for the home page
+// 3. create a route for the comments page
+// 4. create a route for the comments api
+// 5. create a route for the comments form
+// 6. create a route for the comments form submission
+
+// 1. create a web server
+var express = require('express');
+var app = express();
+
+// 2. create a route for the home page
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+// 3. create a route for the comments page
+app.get('/comments', function(request, response) {
+  response.sendFile(__dirname + '/comments.html');
+});
+
+// 4. create a route for the comments api
+app.get('/api/comments', function(request, response) {
+  response.send('this is the comments api');
+});
+
+// 5. create a route for the comments form
+app.get('/comments/new', function(request, response) {
+  response.sendFile(__dirname + '/comments_form.html');
+});
+
+// 6. create a route for the comments form submission
+app.get('/comments/create', function(request, response) {
+  response.send('this is where the form submission will be handled');
+});
+
+// start the web server
+app.listen(8080, function() {
+  console.log('listening on port 8080');
+});
